9bdf34aeb8e93b5434e17b36958b1547e94c3997
Gaëtan Rolé-Dubruille <gaetan.role-dubruille@epitech.eu>
2019-10-10 17:04:59 +0200

    Adding CRUD and bootstrap form theme.

---
+++ src/Controller/MediaController.php
@@ -0,0 +1,94 @@
+<?php
+
+namespace App\Controller;
+
+use App\Entity\Media;
+use App\Form\MediaType;
+use App\Repository\MediaRepository;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+/**
+ * @Route("/media")
+ */
+class MediaController extends AbstractController
+{
+    /**
+     * @Route("/", name="media_index", methods={"GET"})
+     */
+    public function index(MediaRepository $mediaRepository): Response
+    {
+        return $this->render('media/index.html.twig', [
+            'media' => $mediaRepository->findAll(),
+        ]);
+    }
+
+    /**
+     * @Route("/new", name="media_new", methods={"GET","POST"})
+     */
+    public function new(Request $request): Response
+    {
+        $medium = new Media();
+        $form = $this->createForm(MediaType::class, $medium);
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            $entityManager = $this->getDoctrine()->getManager();
+            $entityManager->persist($medium);
+            $entityManager->flush();
+
+            return $this->redirectToRoute('media_index');
+        }
+
+        return $this->render('media/new.html.twig', [
+            'medium' => $medium,
+            'form' => $form->createView(),
+        ]);
+    }
+
+    /**
+     * @Route("/{id}", name="media_show", methods={"GET"})
+     */
+    public function show(Media $medium): Response
+    {
+        return $this->render('media/show.html.twig', [
+            'medium' => $medium,
+        ]);
+    }
+
+    /**
+     * @Route("/{id}/edit", name="media_edit", methods={"GET","POST"})
+     */
+    public function edit(Request $request, Media $medium): Response
+    {
+        $form = $this->createForm(MediaType::class, $medium);
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            $this->getDoctrine()->getManager()->flush();
+
+            return $this->redirectToRoute('media_index');
+        }
+
+        return $this->render('media/edit.html.twig', [
+            'medium' => $medium,
+            'form' => $form->createView(),
+        ]);
+    }
+
+    /**
+     * @Route("/{id}", name="media_delete", methods={"DELETE"})
+     */
+    public function delete(Request $request, Media $medium): Response
+    {
+        if ($this->isCsrfTokenValid('delete'.$medium->getId(), $request->request->get('_token'))) {
+            $entityManager = $this->getDoctrine()->getManager();
+            $entityManager->remove($medium);
+            $entityManager->flush();
+        }
+
+        return $this->redirectToRoute('media_index');
+    }
+}
---
+++ src/Form/MediaType.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace App\Form;
+
+use App\Entity\Media;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+
+class MediaType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options)
+    {
+        $builder
+            ->add('title')
+            ->add('slug')
+            ->add('description')
+            ->add('image')
+            ->add('isSerie')
+            ->add('createdAt')
+            ->add('updatedAt')
+            ->add('isActive')
+            ->add('tags')
+        ;
+    }
+
+    public function configureOptions(OptionsResolver $resolver)
+    {
+        $resolver->setDefaults([
+            'data_class' => Media::class,
+        ]);
+    }
+}
